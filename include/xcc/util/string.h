4678efc20c6c3c03e89b3581d25b3e2be554b96b
maxrt <max.r.tkachuk@gmail.com>
2025-01-09 04:13:23 +0200

    Added string escape sequences

--- include/xcc/util/string.h
+++ include/xcc/util/string.h
@@ -7,4 +7,6 @@ namespace xcc::util {
 
 std::vector<std::string> strsplit(const std::string& str, const std::string& delimiter = " ");
 
+void strreplace(std::string& str, const std::string& from, const std::string& to);
+
 }
