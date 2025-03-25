69d9670bdb8bac5fe74d5df751c5923b3febaa69
maxrt <max.r.tkachuk@gmail.com>
2025-03-26 01:07:42 +0200

    Refactor ast.cc

--- src/ast.cc
+++ src/ast.cc
@@ -8,43 +8,10 @@ using namespace xcc::ast;
 
 static auto logger = xcc::util::log::Logger("AST");
 
-static std::string operator_str_from_token_type(TokenType type) {
-  switch (type) {
-    case TokenType::TOKEN_PLUS:               return "+";
-    case TokenType::TOKEN_MINUS:              return "-";
-    case TokenType::TOKEN_SLASH:              return "/";
-    case TokenType::TOKEN_STAR:               return "*";
-    case TokenType::TOKEN_EQUALS:             return "=";
-    case TokenType::TOKEN_ADD_EQUALS:         return "+=";
-    case TokenType::TOKEN_MIN_EQUALS:         return "-=";
-    case TokenType::TOKEN_MUL_EQUALS:         return "*=";
-    case TokenType::TOKEN_DIV_EQUALS:         return "/=";
-    case TokenType::TOKEN_AND_EQUALS:         return "&=";
-    case TokenType::TOKEN_OR_EQUALS:          return "|=";
-    case TokenType::TOKEN_LOGICAL_AND_EQUALS: return "&&=";
-    case TokenType::TOKEN_LOGICAL_OR_EQUALS:  return "||=";
-    case TokenType::TOKEN_EQUALS_EQUALS:      return "==";
-    case TokenType::TOKEN_NOT_EQUALS:         return "!=";
-    case TokenType::TOKEN_LESS:               return "<";
-    case TokenType::TOKEN_GREATER:            return ">";
-    case TokenType::TOKEN_LESS_EQUALS:        return "<=";
-    case TokenType::TOKEN_GREATER_EQUALS:     return ">=";
-    case TokenType::TOKEN_AND:                return "&&";
-    case TokenType::TOKEN_OR:                 return "||";
-    case TokenType::TOKEN_NOT:                return "!";
-    case TokenType::TOKEN_AMP:                return "&";
-    default:                                  return "<?>";
-  }
-}
-
 static void print_indent(int indent) {
   for (int i = 0; i < indent; ++i) {
     logger.print(" ");
   }
-}
-
-static bool node_any_of(NodeType type, const std::vector<NodeType>& match) {
-  return std::any_of(match.begin(), match.end(), [&](auto t) { return t == type; });
 }
 
 static void print_node(Node* node, Node* parent, int indent) {
@@ -98,14 +65,14 @@ static void print_node(Node* node, Node* parent, int indent) {
     case AST_EXPR_BINARY: {
       auto cast = node->as<Binary>();
       print_node(cast->lhs.get(), cast, indent);
-      logger.print(" %s ", operator_str_from_token_type(cast->operation.type).c_str());
+      logger.print(" %s ", cast->operation.toString().c_str());
       print_node(cast->rhs.get(), cast, indent);
       break;
     }
 
     case AST_EXPR_UNARY: {
       auto cast = node->as<Unary>();
-      logger.print("%s", operator_str_from_token_type(cast->operation.type).c_str());
+      logger.print("%s", cast->operation.toString().c_str());
       print_node(cast->rhs.get(), cast, indent);
       break;
     }
@@ -160,24 +127,27 @@ static void print_node(Node* node, Node* parent, int indent) {
     case AST_EXPR_ASSIGN: {
       auto assign = node->as<Assign>();
       print_node(assign->lhs.get(), assign, indent);
-      logger.print(" %s ", operator_str_from_token_type(assign->kind.type).c_str());
+      logger.print(" %s ", assign->kind.toString().c_str());
       print_node(assign->rhs.get(), assign, indent);
       break;
     }
 
     case AST_BLOCK: {
       auto block = node->as<Block>();
-      print_indent(indent);
-      logger.print("{\n");
+      // print_indent(indent);
+      logger.print(" {\n");
       for (auto& stmt : block->body) {
         print_indent(indent + 2);
         print_node(stmt.get(), block, indent + 2);
-        if (!node_any_of(stmt->type, {AST_BLOCK, AST_FUNCTION_DECL, AST_FUNCTION_DEF, AST_IF, AST_FOR, AST_WHILE, AST_STRUCT})) {
+        if (!stmt->isAnyOf(AST_BLOCK, AST_FUNCTION_DECL, AST_FUNCTION_DEF, AST_IF, AST_FOR, AST_WHILE, AST_STRUCT)) {
           logger.print(";\n");
         }
       }
       print_indent(indent);
-      logger.print("}\n");
+      logger.print("}");
+      if (!parent->isAnyOf(AST_IF, AST_FOR, AST_WHILE)) {
+        logger.print("\n");
+      }
       break;
     }
 
@@ -211,9 +181,8 @@ static void print_node(Node* node, Node* parent, int indent) {
       logger.print("): ");
       print_node(fndecl->return_type.get(), fndecl, indent);
       if (!parent->is(AST_FUNCTION_DEF)) {
-        logger.print(";");
+        logger.print(";\n");
       }
-      logger.print("\n");
       break;
     }
 
@@ -243,13 +212,13 @@ static void print_node(Node* node, Node* parent, int indent) {
       auto if_stmt = node->as<If>();
       logger.print("if (");
       print_node(if_stmt->condition.get(), if_stmt, indent);
-      logger.print(")\n");
+      logger.print(") ");
       print_node(if_stmt->then_branch.get(), if_stmt, indent);
       if (if_stmt->else_branch) {
-        print_indent(indent);
-        logger.print("else\n");
+        logger.print(" else");
         print_node(if_stmt->else_branch.get(), if_stmt, indent);
       }
+      logger.print("\n");
       break;
     }
 
@@ -261,8 +230,9 @@ static void print_node(Node* node, Node* parent, int indent) {
       print_node(for_stmt->cond.get(), for_stmt, indent);
       logger.print("; ");
       print_node(for_stmt->step.get(), for_stmt, indent);
-      logger.print(")\n");
+      logger.print(") ");
       print_node(for_stmt->body.get(), for_stmt, indent);
+      logger.print("\n");
       break;
     }
 
@@ -270,7 +240,7 @@ static void print_node(Node* node, Node* parent, int indent) {
       auto while_stmt = node->as<While>();
       logger.print("while (");
       print_node(while_stmt->condition.get(), while_stmt, indent);
-      logger.print(")\n");
+      logger.print(") ");
       print_node(while_stmt->body.get(), while_stmt, indent);
       break;
     }
