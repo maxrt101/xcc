b50fe0a2419236a367a7b062ae6dba0a62db7577
maxrt <max.r.tkachuk@gmail.com>
2025-06-06 03:09:59 +0300

    Fix strsplit

--- src/util/string.cc
+++ src/util/string.cc
@@ -2,12 +2,25 @@
 
 std::vector<std::string> xcc::util::strsplit(const std::string& str, const std::string& delimiter) {
   std::vector<std::string> result;
+
   size_t last = 0, next = 0;
+
   while ((next = str.find(delimiter, last)) != std::string::npos) {
-    result.push_back(str.substr(last, next-last));
+    auto sub = str.substr(last, next-last);
+
+    if (!sub.empty()) {
+      result.push_back(sub);
+    }
+
     last = next + 1;
   }
-  result.push_back(str.substr(last));
+
+  auto sub = str.substr(last);
+
+  if (!sub.empty()) {
+    result.push_back(sub);
+  }
+
   return result;
 }
 
