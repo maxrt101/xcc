d8d9466da14a05e9c3c3a88dc5c69c0a58f25462
maxrt <max.r.tkachuk@gmail.com>
2025-06-08 02:27:35 +0300

    Fix 'if' (no else branch, returning from one of the branches, etc)

--- src/ast/if.cc
+++ src/ast/if.cc
@@ -14,11 +14,7 @@ std::shared_ptr<If> If::create(std::shared_ptr<Node> condition, std::shared_ptr<
 }
 
 llvm::Value * If::generateValue(codegen::ModuleContext& ctx, PayloadList payload) {
-  auto cond_val = throwIfNull(condition->generateValue(ctx, {}), CodegenException("if condition generated NULL"));
-
-  if (!cond_val) {
-    throw CodegenException("Error generating condition of 'if' statement");
-  }
+  auto cond_val = throwIfNull(condition->generateValue(ctx, {}), CodegenException("Error generating condition of 'if' statement (condition generated NULL)"));
 
   auto then_type = throwIfNull(then_branch->generateType(ctx, {}), CodegenException("if then branch generated NULL type"));
   auto else_type = meta::Type::createVoid();
@@ -41,19 +37,15 @@ llvm::Value * If::generateValue(codegen::ModuleContext& ctx, PayloadList payload
 
   auto fn = ctx.ir_builder->GetInsertBlock()->getParent();
 
-  auto then_block = llvm::BasicBlock::Create(*ctx.llvm.ctx, "then", fn);
-  auto else_block = llvm::BasicBlock::Create(*ctx.llvm.ctx, "else");
+  auto then_block  = llvm::BasicBlock::Create(*ctx.llvm.ctx, "then", fn);
+  auto else_block  = llvm::BasicBlock::Create(*ctx.llvm.ctx, "else");
   auto merge_block = llvm::BasicBlock::Create(*ctx.llvm.ctx, "ifcont");
 
   ctx.ir_builder->CreateCondBr(cond_val, then_block, else_block);
 
   // If then branch
   ctx.ir_builder->SetInsertPoint(then_block);
-  auto then_val = throwIfNull(then_branch->generateValue(ctx, {}), CodegenException("if then branch generated NULL"));
-
-  if (!then_val) {
-    throw CodegenException("Error generating 'then' block of 'if' statement");
-  }
+  auto then_val = throwIfNull(then_branch->generateValue(ctx, {}), CodegenException("Error generating 'then' block of 'if' statement (then branch generated NULL)"));
 
   then_val = codegen::castIfNotSame(ctx, then_val, common_type->getLLVMType(ctx));
 
@@ -66,33 +58,49 @@ llvm::Value * If::generateValue(codegen::ModuleContext& ctx, PayloadList payload
   // If else branch
   fn->insert(fn->end(), else_block);
   ctx.ir_builder->SetInsertPoint(else_block);
-
-  auto else_val = else_branch ? throwIfNull(else_branch->generateValue(ctx, {}), CodegenException("If else branch generated NULL")) : nullptr;
+  auto else_val = else_branch ? throwIfNull(else_branch->generateValue(ctx, {}), CodegenException("Error generating 'then' block of 'if' statement (else branch generated NULL)")) : nullptr;
 
   if (else_val) {
     else_val = codegen::castIfNotSame(ctx, else_val, common_type->getLLVMType(ctx));
+
+    // Generate branch to merge block if else_branch doesn't return from the function
     if (!isOrIsLastInBlock(else_branch, AST_RETURN)) {
       ctx.ir_builder->CreateBr(merge_block);
     }
   } else {
+    // Generate default value for 'else' if else block in AST is empty
+    if (!common_type->isVoid()) {
+      else_val = common_type->getDefault(ctx);
+    }
     ctx.ir_builder->CreateBr(merge_block);
   }
-
 
   else_block = ctx.ir_builder->GetInsertBlock();
 
   // Merge block (after then & else)
   fn->insert(fn->end(), merge_block);
   ctx.ir_builder->SetInsertPoint(merge_block);
-  auto phi = ctx.ir_builder->CreatePHI(common_type->getLLVMType(ctx), 2, "iftmp");
 
-  phi->addIncoming(then_val, then_block);
+  // Generate phi only if common type isn't void, as it's illegal to have void as phi type
+  // And it's not required to generate phi if it's not possible for the blocks to converge at
+  // merge_block, as they return from the function
+  if (!common_type->isVoid()) {
+    auto phi = ctx.ir_builder->CreatePHI(common_type->getLLVMType(ctx), 2, "iftmp");
 
-  if (else_val) {
-    phi->addIncoming(else_val, else_block);
+    // Add then block to phy if it doesn't return from the function
+    if (!llvm::isa<llvm::ReturnInst>(then_block->getTerminator())) {
+      phi->addIncoming(then_val, then_block);
+    }
+
+    // Add else block to phy if it doesn't return from the function
+    if (!llvm::isa<llvm::ReturnInst>(else_block->getTerminator())) {
+      phi->addIncoming(else_val, else_block);
+    }
+
+    return phi;
   }
 
-  return phi;
+  return nullptr;
 }
 
 std::shared_ptr<xcc::meta::Type> If::generateType(codegen::ModuleContext& ctx, PayloadList payload) {
