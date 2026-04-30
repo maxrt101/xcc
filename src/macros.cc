73609644ee36d7fe9dc40ee85826133145637c9e
maxrt <max.r.tkachuk@gmail.com>
2026-04-30 04:40:39 +0300

    Add cond! macro & fix argument type checking and retrieval

--- src/macros.cc
+++ src/macros.cc
@@ -3,21 +3,25 @@
 #include "xcc/codegen.h"
 #include "xcc/util/log.h"
 
-#define __GET_NUM_VAL(__n) (__n->tag == Number::FLOATING ? __n->value.floating : __n->value.integer)
+#define __GET_NUM_VAL(__n) \
+  (__n->tag == Number::FLOATING ? __n->value.floating : __n->value.integer)
 
-#define __BIN_OP(__name, __ctx, __call, __op)                                                                           \
-  assertThrow(__call->args[0]->is(AST_EXPR_NUMBER), CodegenException("Expected number for first argument to " __name)); \
-  assertThrow(__call->args[1]->is(AST_EXPR_NUMBER), CodegenException("Expected number for second argument to " __name));\
-  auto a1 = __call->args[0]->as<Number>();                                                                              \
-  auto a2 = __call->args[1]->as<Number>();                                                                              \
-  bool is_float = a1->tag == Number::FLOATING || a2->tag == Number::FLOATING;                                           \
-  double result = __GET_NUM_VAL(a1) __op __GET_NUM_VAL(a2);                                                             \
+#define __BIN_OP(__name, __ctx, __call, __op)                                                                         \
+  assertThrow(isOrIsLastInBlock(__call->args[0], AST_EXPR_NUMBER),                                                    \
+    CodegenException("Expected number for first argument to " __name));                                               \
+  assertThrow(isOrIsLastInBlock(__call->args[1], AST_EXPR_NUMBER),                                                    \
+    CodegenException("Expected number for second argument to " __name));                                              \
+  auto a1 = getOrGetLastInBlock(__call->args[0], AST_EXPR_NUMBER)->as<Number>();                                      \
+  auto a2 = getOrGetLastInBlock(__call->args[1], AST_EXPR_NUMBER)->as<Number>();                                      \
+  bool is_float = a1->tag == Number::FLOATING || a2->tag == Number::FLOATING;                                         \
+  double result = __GET_NUM_VAL(a1) __op __GET_NUM_VAL(a2);                                                           \
   return is_float ? Number::createFloating(result) : Number::createInteger(result);
 
-#define __UN_OP(__name, __ctx, __call, __op)                                                                            \
-  assertThrow(__call->args[0]->is(AST_EXPR_NUMBER), CodegenException("Expected number as argument to " __name));        \
-  auto a = __call->args[0]->as<Number>();                                                                               \
-  assertThrow(a->tag == Number::INTEGER, CodegenException("Expected integer as argument to " __name));                  \
+#define __UN_OP(__name, __ctx, __call, __op)                                                                          \
+  assertThrow(isOrIsLastInBlock(__call->args[0], AST_EXPR_NUMBER),                                                    \
+    CodegenException("Expected number as argument to " __name));                                                      \
+  auto a = getOrGetLastInBlock(__call->args[0], AST_EXPR_NUMBER)->as<Number>();                                       \
+  assertThrow(a->tag == Number::INTEGER, CodegenException("Expected integer as argument to " __name));                \
   return Number::createInteger(__op a->value.integer);
 
 using namespace xcc;
@@ -141,8 +145,8 @@ static std::shared_ptr<Node> xcc_macro_strf(Macro::NativeContext& ctx, std::shar
 }
 
 static std::shared_ptr<Node> xcc_macro_int(Macro::NativeContext& ctx, std::shared_ptr<MacroCall> call) {
-  assertThrow(call->args[0]->is(AST_EXPR_STRING), CodegenException("int! expects a string as an argument"));
-  auto s = call->args[0]->as<String>();
+  assertThrow(isOrIsLastInBlock(call->args[0], AST_EXPR_STRING), CodegenException("int! expects a string as an argument"));
+  auto s = getOrGetLastInBlock(call->args[0], AST_EXPR_STRING)->as<String>();
 
   if (s->value.find('.') != std::string::npos) {
     return Number::createFloating(std::stod(s->value));
@@ -169,19 +173,30 @@ static std::shared_ptr<Node> xcc_macro_dec(Macro::NativeContext& ctx, std::share
   __UN_OP("dec!", ctx, call, --);
 }
 
-static std::vector builtin_macros = {
-  createNativeMacro("cat",     {"a", "b"}, xcc_macro_cat),
-  createNativeMacro("sizeof",  {"expr"},   xcc_macro_sizeof),
-  createNativeMacro("typeof",  {"expr"},   xcc_macro_typeof),
-  createNativeMacro("is_same", {"a", "b"}, xcc_macro_is_same),
-  createNativeMacro("str",     {"expr"},   xcc_macro_str),
-  createNativeMacro("strf",    {"expr"},   xcc_macro_strf),
-  createNativeMacro("int",     {"expr"},   xcc_macro_int),
+static std::shared_ptr<Node> xcc_macro_cond(Macro::NativeContext& ctx, std::shared_ptr<MacroCall> call) {
+  assertThrow(isOrIsLastInBlock(call->args[0], AST_EXPR_NUMBER), CodegenException("cond! expects a number as first argument"));
 
-  createNativeMacro("add",     {"a", "b"}, xcc_macro_add),
-  createNativeMacro("sub",     {"a", "b"}, xcc_macro_sub),
-  createNativeMacro("inc",     {"x"},      xcc_macro_inc),
-  createNativeMacro("dec",     {"x"},      xcc_macro_dec),
+  auto cond = getOrGetLastInBlock(call->args[0], AST_EXPR_NUMBER)->as<Number>();
+  assertThrow(cond->tag == Number::INTEGER, CodegenException("Expected integer as first argument to cond!"));
+
+  return (cond->value.integer ? call->args[1] : call->args[2])->clone();
+}
+
+static std::vector builtin_macros = {
+  createNativeMacro("cat",     {"a", "b"},               xcc_macro_cat),
+  createNativeMacro("sizeof",  {"expr"},                 xcc_macro_sizeof),
+  createNativeMacro("typeof",  {"expr"},                 xcc_macro_typeof),
+  createNativeMacro("is_same", {"a", "b"},               xcc_macro_is_same),
+  createNativeMacro("str",     {"expr"},                 xcc_macro_str),
+  createNativeMacro("strf",    {"expr"},                 xcc_macro_strf),
+  createNativeMacro("int",     {"expr"},                 xcc_macro_int),
+
+  createNativeMacro("add",     {"a", "b"},               xcc_macro_add),
+  createNativeMacro("sub",     {"a", "b"},               xcc_macro_sub),
+  createNativeMacro("inc",     {"x"},                    xcc_macro_inc),
+  createNativeMacro("dec",     {"x"},                    xcc_macro_dec),
+
+  createNativeMacro("cond",    {"cond", "then", "else"}, xcc_macro_cond),
 };
 
 void codegen::registerBuiltinMacros(GlobalContext& ctx) {
