2154d430d41a1a153002bd89ab0b92dd681b37f6
maxrt <max.r.tkachuk@gmail.com>
2026-05-24 03:45:26 +0300

    Add some utilities

--- include/xcc/util/util.h
+++ include/xcc/util/util.h
@@ -1,5 +1,24 @@
 #pragma once
 
+#include <vector>
+#include <string>
+
+/**
+ * Shorthand for checking if a value is contained by a vector
+ *
+ * @code{.c}
+ *   std::vector<std::string> values;
+ *   if (XCC_VECTOR_CONTAINS(values, "abc")) {
+ *     // "abc" is in values
+ *   }
+ * @endcode
+ *
+ * @param __vec     Vector to find __element in
+ * @param __element Element to find in __vec
+ */
+#define XCC_VECTOR_CONTAINS(__vec, __element) \
+  (std::find(__vec.begin(), __vec.end(), __element) != __vec.end())
+
 /**
  * Shorthand for finding a value in a vector
  *
@@ -14,7 +33,7 @@
  * @param __element Element to find in __vec
  * @param __it      Variable name for iterator
  */
-#define XCC_FIND_IN_VECTOR(__vec, __element, __it)            \
+#define XCC_IF_VECTOR_CONTAINS(__vec, __element, __it)        \
   auto __it = std::find_if(__vec.begin(), __vec.end(),        \
     [&](const auto& x) { return x == __element; });           \
   if (__it != __vec.end())
@@ -25,7 +44,7 @@
  *
  * @code{.c}
  *   std::vector<std::pair<int, std::string>> values;
- *   XCC_FIND_IN_VECTOR(values, 42, v) {
+ *   XCC_IF_VECTOR_CONTAINS_BY_PAIR0(values, 42, v) {
  *     // v is an iterator to std::pair here
  *   }
  * @endcode
@@ -34,9 +53,9 @@
  * @param __element Element to find in __vec
  * @param __it      Variable name for iterator
  */
-#define XCC_FIND_IN_VECTOR_BY_PAIR0(__vec, __element, __it)   \
-  auto __it = std::find_if(__vec.begin(), __vec.end(),        \
-    [&](const auto& x) { return x.first == __element; });     \
+#define XCC_IF_VECTOR_CONTAINS_BY_PAIR0(__vec, __element, __it)   \
+  auto __it = std::find_if(__vec.begin(), __vec.end(),            \
+    [&](const auto& x) { return x.first == __element; });         \
   if (__it != __vec.end())
 
 /**
@@ -45,7 +64,7 @@
  *
  * @code{.c}
  *   std::vector<std::pair<int, std::string>> values;
- *   XCC_FIND_IN_VECTOR(values, "abc", v) {
+ *   XCC_IF_VECTOR_CONTAINS_BY_PAIR1(values, "abc", v) {
  *     // v is an iterator to std::pair here
  *   }
  * @endcode
@@ -54,18 +73,73 @@
  * @param __element Element to find in __vec
  * @param __it      Variable name for iterator
  */
-#define XCC_FIND_IN_VECTOR_BY_PAIR1(__vec, __element, __it)   \
-  auto __it = std::find_if(__vec.begin(), __vec.end(),        \
-    [&](const auto& x) { return x.second == __element; });    \
+#define XCC_IF_VECTOR_CONTAINS_BY_PAIR1(__vec, __element, __it)   \
+  auto __it = std::find_if(__vec.begin(), __vec.end(),            \
+    [&](const auto& x) { return x.second == __element; });        \
   if (__it != __vec.end())
 
 
 namespace xcc {
 
-/* Checks of value is in any of args */
+/**
+ * Checks if `value` is one of the `args`
+ *
+ * @tparam T     `value` type
+ * @tparam Args  `args` pack type. Each individual element should be comparable to `T`
+ * @param  value Value to check if present in `args`
+ * @param  args  Pack to check if `value` is present in
+ * @return @c true if `value` is in `args`
+ */
 template <typename T, typename... Args>
 bool oneOf(T&& value, Args&&... args) {
   return ((value == args) || ...);
+}
+
+/**
+ * Merge two vectors together
+ *
+ * @tparam T Vector value type
+ * @param  first Vector to merge into
+ * @param  second Vector to merge
+ * @return first + second
+ */
+template <typename T>
+std::vector<T> mergeVectors(std::vector<T> first, const std::vector<T>& second) {
+  first.insert(first.end(), second.begin(), second.end());
+  return first;
+}
+
+/**
+ * Return a vector, that is a subvector of `vec` with range [start; end]
+ *
+ * @tparam T Vector value type
+ * @param  vec Vector to get subvector from
+ * @param  start Start index
+ * @param  end End index (if 0 - `start` is set to 0 and `end` is set to `start`)
+ * @return `vec[start; end]`
+ */
+template <typename T>
+std::vector<T> sliceVector(const std::vector<T>& vec, ssize_t start, ssize_t end = 0) {
+  if (!end) {
+    end = start;
+    start = 0;
+  }
+
+  if (start < 0) {
+    start = (ssize_t)vec.size() + start;
+  }
+
+  if (end < 0) {
+    end = (ssize_t)vec.size() + end;
+  }
+
+  std::vector<T> res;
+
+  for (ssize_t i = start; i < end; ++i) {
+    res.push_back(vec[i]);
+  }
+
+  return res;
 }
 
 }
